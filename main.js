1908e2e504904c84f7f4979364cc33c49be012cd
manasamancharla <manasamancharla11@protonmail.com>
2024-08-19 15:25:49 +0530

    added custom function

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+import { Client, Databases } from "node-appwrite";
+
+const PROJECT_ID = process.env.PROJECT_ID;
+const DB_ID = process.env.DB_ID;
+const COLLECTION_ID = process.env.COLLECTION_ID;
+
 export default async ({ req, res, log, error }) => {
+	const client = new Client();
+
+	client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
+
+	const db = new Databases(client);
+
+	if (req.method == "GET") {
+		const response = await db.listDocuments(DB_ID, COLLECTION_ID);
+
+		return res.json(response.documents);
+	}
+
 	return res.send("Hello");
 };
