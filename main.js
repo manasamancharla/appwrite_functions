4c811ede08c54fc4c1106c083a9b176eeeba5461
manasamancharla <manasamancharla11@protonmail.com>
2024-08-20 15:16:06 +0530

    --Sendgrid trial--

--- main.js
+++ main.js
@@ -13,59 +13,17 @@ const DB_ID = process.env.DB_ID;
 const COLLECTION_ID = process.env.COLLECTION_ID;
 
 export default async ({ req, res, log, error }) => {
-	if (req.method == "POST") {
-		const { email, name, phone } = JSON.parse(req.payload);
-		const client = new Client();
+	const client = new Client();
 
-		client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
-		// .setKey()
+	client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
 
-		// const db = new Databases(client);
+	const db = new Databases(client);
 
-		// if (req.method == "GET") {
-		// 	const response = await db.listDocuments(DB_ID, COLLECTION_ID);
+	if (req.method == "GET") {
+		const response = await db.listDocuments(DB_ID, COLLECTION_ID);
 
-		// 	return res.json(response.documents);
-		// }
-
-		// const users = new Users(client);
-		// const headers = req.headers;
-		// const userId = headers["x-appwrite-user-id"];
-
-		// const result = await users.createTarget(
-		// 	userId,
-		// 	ID.unique(), // targetId
-		// 	MessagingProviderType.Email, // providerType
-		// 	"email", // identifier
-		// 	"<PROVIDER_ID>", // providerId (optional)
-		// 	"<NAME>" // name (optional)
-		// );
-
-		sgMail.setApiKey(process.env.SENDGRID_KEY);
-
-		const msg = {
-			to: email,
-			from: "amancharlamanas@gmail.com",
-			subject: `Welcome ${name}`,
-			text: `Welcome ${name}`,
-			html: `<p>Welcome ${name}}</p>`,
-		};
-
-		try {
-			await sgMail.send(msg);
-			res.json({ status: "Email sent successfully via SendGrid" });
-		} catch (err) {
-			console.error("SendGrid error:", err);
-			res.json({ status: "Error sending email via SendGrid", error: err });
-		}
-	} else {
-		// res.json({
-		// 	status: "Invalid request method",
-		// 	error: `Expected POST, received ${req.method}`,
-		// });
-
-		res.send(`Expected POST, received ${req.method}`);
+		return res.json(response.documents);
 	}
 
-	// return res.send("Hello");
+	return res.send("Hello");
 };
